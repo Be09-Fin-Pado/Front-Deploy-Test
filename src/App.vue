a98ece1639d5dd955760b3750c2eb4aa52f7d2c0
woodart8 <kibeom1145@gmail.com>
2024-12-09 15:53:46 +0900

    deploy test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
-<script setup>
-
-</script>
-
 <template>
-  <p>테스트</p>
+  <div>
+    <input type="text" v-model="test" placeholder=".shop주소 도메인명을 쓰시오!!!!">
+    <button v-on:click="move">이동</button>
+  </div>
 </template>
+
+<script setup>
+import {ref} from 'vue';
+
+const test = ref('');
+
+const move = () => {
+  location.href='http://' + test.value + '.shop/health';
+}
+</script>
 
 <style scoped>
 
